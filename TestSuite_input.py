3a3ebd58591c33fc7bef5beea9fbffada418b44a
Jhan Srbinovsky <jhan.srbinovsky@gmail.com>
2014-07-15 14:35:22 +1000

    works - diags, commented code left in

--- TestSuite_input.py
+++ TestSuite_input.py
@@ -1,97 +1,75 @@
+#/usr/bin/python 
 import sys, getopt
+
 #Basically we have to read a config file and write namelists accordingly
 ###############################################################################
 
-def CLI_interpreter( argv,
-                     config,
-                     flags ):
-
-    # get ALL opts & args - where hmfxn are valid options
-    # (possible values for "opt") & ":" following valid options
-    # represents there is an "arg" to follow
-    try:
-        #opts, args = getopt.getopt(argv, "hm:f:x:n:", ["ifile=","ofile="])
-        opts, args = getopt.getopt(argv, "hm:f:n:")
-    # if none of these are present - abort
-    except getopt.GetoptError:
-        sys.exit(2)
-
-    # Loop over all the options read in, & associate argument
-    for opt, arg in opts:
-
-        # if the user wants help (NO arg)
-        if opt in ('-h', "--help"):
-            #jhan: clean up reportage
-            print 'TestSuite.py -f <config_file>\n'
-            sys.exit()
-
-        # user config file (arg = <filename>) only valid from CLI
-        elif opt in ("-f", "--configfile"):
-            config.file.append( arg )
-            # reset 1st element indicating CLI arg present
-            flags.flag[0] = True
-        
-    print "CLI requires a config file as an argument."
+def Locate_cfg( argv, ifile, ofile ):
+   inputfile = 'default.cfg'
+   outputfile = 'default.log'
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print 'test.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'test.py -i <inputfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   
+   ifile.append( inputfile )
+   ofile.append( outputfile )
+   #print 'Input file is "', inputfile
+   #print 'Output file is "', outputfile
 
 ###############################################################################
 
-def Configfile_interpreter( config,
-                            fields,
-                            flags,
-                            pars ):
+def Configfile_interpreter( ifile, cfg ):
 
-    # Declare mutable object => lines read from config file 
-    lines = []
+   # Declare mutable object => lines read from config file 
+   lines = []
+   
+   # Reads all the lines in the config file specified at CLI
+   lines = tuple( open(ifile[0], 'r' ))
+   
+   # Loop over all of these read lines
+   for ll in range( len(lines) ):
+   
+      # Create a string from each line
+      lstr = str(lines[ll])
+      # nullify case sensitivity in that string
+      lstr = lstr.lower()
+      
+      # if it is meant to be interpreted - interpret based on what
+      # it starts with. See configfile example format
+      # check if the string is a comment
+      if not lstr.startswith('#'):
+      
+         # Mode
+         if lstr.startswith( "mode" ):
+            cfield = lines[ll].strip().split()
+            cfg.mode.append(cfield[1])
+         
+         # Name
+         if lstr.startswith( "name" ):
+            cfield = lines[ll].strip().split()
+            cstr = ""
+            for istr in range( 1,len(cfield) ):
+               cstr = cstr + cfield[istr] 
+            cfg.name.append(cstr)
+         
+         # Path
+         if lstr.startswith( "path" ):
+            cfield = lines[ll].strip().split()
+            cfg.mode.append(cfield[1])
+      
+      # End IF not comment line
 
-    npaths = 0
-    # Reads all the lines in the config file specified at CLI
-    lines = tuple( open(config.file[0], 'r' ))
-
-    # Loop over all of these read lines
-    for ll in range( len(lines) ):
-
-        # Create a string from each line
-        lstr = str(lines[ll])
-        # nullify case sensitivity in that string
-        lstr = lstr.lower()
-
-        # if it is meant to be interpreted - interpret based on what
-        # it starts with. See configfile example format
-        # check if the string is a comment
-        if not lstr.startswith('#'):
-
-            # fieldnames
-            if lstr.startswith( pars.strchk[0] ):
-                cfield = lines[ll].strip().split()
-                fields.name.append(cfield[1])
-
-            # paths
-            if lstr.startswith( pars.strchk[1] ):
-                npaths=npaths+1
-                cfield = lines[ll].strip().split()
-                fields.path.append(cfield[1])
-
-            # nodes
-            if lstr.startswith( pars.strchk[2] ):
-                cfield = lines[ll].strip().split()
-                config.nodes.append(cfield[1])
-                flags.flag[2] = True
-
-            # mapping
-            if lstr.startswith( pars.strchk[3] ):
-                cfield = lines[ll].strip().split()
-                config.map.append(cfield[1])
-
-    # End Loop over read lines[]
-
-    if npaths > 1:
-        flags.flag[3] = True
-    
-    if flags.flag[2] is False:
-        print "Aborting. Nodes NOT specified."
-        sys.exit()        
-
-    print "Config file Interpreted"
+   # End Loop over read lines[]
 
 ###############################################################################
 
--- TestSuite_main.py
+++ TestSuite_main.py
@@ -1,115 +1,101 @@
-#!/usr/bin/python
+#!/usr/bin/python 
 __author__ = 'Jhan Srbinovsky'
 
-# Process binary fields dumped per node from CABLE into global netcdf files.
+# Build & Run various CABLE applications described in config file 
 
-# usage: ./bins2ncdf_main -f <config file>
-
-# Required format for config file described in example configfile
+# usage: ./TestSuite_main -f <config file>
 
 #import python modules
 import sys
 import subprocess
 
 #import local, application specific modules
-from bins2ncdf_input import user_input 
-from bins2ncdf_driver import driver 
+from TestSuite_input import Locate_cfg, Configfile_interpreter
 
 def main(argv):
     
-    # insert a break from the CLI for reading
-    print "\n"
+   # insert a break from the CLI for reading
+   print "\n"
+   
+###############################################################################
+   # Locate config file (ifile), log file (ofile)
+   ifile = [] 
+   ofile = []
+   Locate_cfg( argv, ifile, ofile )
+   #print "ifile ", ifile[0] 
+   #print "ofile ", ofile[0] 
+   
+###############################################################################
+   # Read config file
+   
+   # class to hold all config file input
+   class Configs(object):
+      def __init__(self):
+         # Declare flags as mutable:
+         self.mode = []
+         self.name = []
+         self.path = []
+   cfg = []
+   #for i in range( 1 ):   
+   #   cfg.append( Configs() )
+   cfg = Configs() 
+   
+   # Read config file and fill cfg. class
+   Configfile_interpreter( ifile, cfg )
+   
+   # Print what we are going to do cfg. class
+   print "Applications to be evaluated:" 
+   for i in range( len( cfg.name ) ):   
+      print "\n",cfg.name[i] 
+
+   # print class all at once 
+   #attrs = vars( cfg )
+   # now dump this in some way or another
+   #print ', '.join("%s: %s" % item for item in attrs.items())
+   sys.exit()
+ 
+###############################################################################
     
-    # Defaults defined. Ability to overwrite these is depracated. Therefore 
-    # change here if necessary. Default directories are given relative to ./ 
-
-    class Params(object):
-        def __init__(self):
-
-            # Check per line "label" in configfile defining the type
-            # of the following argument 
-            self.strchk =   [ "FieldName",
-                              "Path",
-                              "Nodes",
-                              "Mapping",
-                              "MapPath" ]
-
-            # Directories: mapping data, cocatenated data, exececutables 
-            self.path =     [ "data/mapping",
-                              "data/catted",
-                              "bin/cat_Nnodes", 
-                              "bin/ncdf_main", 
-                              "data/ncdf" ] 
-
-            # Fieldnames of mapping data 
-            self.mapfield = [ "latitude",
-                              "lat_index",
-                              "lon_index",
-                              "tile_frac",
-                              "tile_index" ]
-
-    # Declare flags as mutable:
-    # [0] - configfile, [1] - mapping, [2] - nodes, [3] - N paths 
-    class Flags(object):
-        def __init__(self):
-            self.flag = []
-
-    class Fields(object):
-        def __init__(self):
-            self.name  = []
-            self.path  = []
-
-    
-    # Can be set either via CLI or commandline: trumped by configfile
-    class Configs(object):
-        def __init__(self):
-            self.file = []      # Can be set only via CLI 
-            self.map  = []
-            self.nodes  = []
-
-    pars = Params()
-    flags = Flags()
-    fields = Fields()
-    config = Configs() 
-
-    #execute system commands
-    print "In current format, dumped files are longitudinally complete per node."
-    print "Therefore cannot be processed by same methodi used here."
-    cmd1 = ("/bin/cp " + pars.path[0] + "/longitude001.bin " 
-           + pars.path[1] + "/longitude.bin")
-    cmd2 = ("/bin/cp "+ pars.path[0] + "/longitude001.dat " 
-           + pars.path[1] + "/longitude.dat")
-
-    #subprocess.call(cmd)
-    p = subprocess.Popen(cmd1, stdout=subprocess.PIPE, shell=True)
-    (output, err) = p.communicate()
-    p = subprocess.Popen(cmd2, stdout=subprocess.PIPE, shell=True)
-    (output, err) = p.communicate()
-    
-    # Get user input from both CLI and config file
-    user_input( argv,
-                config,
-                pars,
-                fields,
-                flags )
-    
-    # Operate on data
-    driver(     fields,
-                pars,
-                config,
-                flags )
+   
+   #execute system commands
+   print "In current format, dumped files are longitudinally complete per node."
+   print "Therefore cannot be processed by same methodi used here."
+   cmd1 = ("/bin/cp " + pars.path[0] + "/longitude001.bin " 
+          + pars.path[1] + "/longitude.bin")
+   cmd2 = ("/bin/cp "+ pars.path[0] + "/longitude001.dat " 
+          + pars.path[1] + "/longitude.dat")
+   
+   #subprocess.call(cmd)
+   p = subprocess.Popen(cmd1, stdout=subprocess.PIPE, shell=True)
+   (output, err) = p.communicate()
+   p = subprocess.Popen(cmd2, stdout=subprocess.PIPE, shell=True)
+   (output, err) = p.communicate()
+   
+   # Get user input from both CLI and config file
+   user_input( argv,
+               config,
+               pars,
+               fields,
+               flags )
+   
+   # Operate on data
+   driver(     fields,
+               pars,
+               config,
+               flags )
+   
+   
+   
+   
+   
+   
 
 
 
 
 
-
-
-
-
-
-
-################################################################################
+# comment out if interactive
+#################################################################################
 if __name__ == "__main__":
    main(sys.argv[1:])
 
