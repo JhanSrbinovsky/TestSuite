7e52027629984da39be9bef2187b9f58f2499cfe
JhanSrbinovsky <jxs599@raijin5.(none)>
2014-08-06 07:55:59 +1000

    rm directory

--- jaaad/ummodel/ppsrc/UM/utility/qxfieldcos/cray2vax.f90
+++
@@ -1,9 +0,0 @@
-
-
-      INTEGER FUNCTION CRAY2VAX(I,LEN_RLABEL,                           &
-     &                          VAX_LABEL,BIT_OFF,RLABEL)
-      INTEGER I,LEN_RLABEL,BIT_OFF,VAX_LABEL
-      REAL RLABEL
-      CRAY2VAX=-1
-      RETURN
-      END FUNCTION CRAY2VAX
--- jaaad/umrecon/ppsrc/UM/control/mpp/swapable_field_mod.f90
+++
@@ -1,9 +0,0 @@
-    MODULE Swapable_Field_Mod
-      TYPE swapable_field_pointer_type
-        INTEGER :: field_type
-        INTEGER :: levels
-        INTEGER :: rows
-        LOGICAL :: vector
-        REAL, POINTER :: field(:, :, :)
-      END TYPE swapable_field_pointer_type
-    END MODULE Swapable_Field_Mod
