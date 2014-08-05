fd9ff3f36f639148dc1fd4cfb363ec629b3953b8
JhanSrbinovsky <jxs599@raijin5.(none)>
2014-08-06 07:55:21 +1000

    rm directory

--- jaaad/ummodel/fcm_env.ksh
+++
@@ -1,1 +0,0 @@
-/short/p66/jxs599/UM_ROUTDIR/jxs599/jaaad/ummodel/fcm_env.sh
--- jaaad/ummodel/fcm_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-PATH=/short/p66/jxs599/UM_ROUTDIR/jxs599/jaaad/ummodel/bin:/short/p66/jxs599/UM_ROUTDIR/jxs599/jaaad/umbase/bin:$PATH
-export PATH
